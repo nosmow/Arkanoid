7b405499d0fbd6fc53ea0f88c1c8552f144c09d3
nosmow <nosmow.dev@gmail.com>
2024-10-30 17:18:31 -0500

    Basic player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float movX = Input.GetAxisRaw("Horizontal");
+        moveDirection = new Vector3(movX, 0, 0).normalized * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (moveDirection != Vector3.zero)
+        {
+            //Vector3 newPostion = rb.
+            rb.MovePosition(rb.position + moveDirection * Time.fixedDeltaTime);
+        }
+    }
+}
