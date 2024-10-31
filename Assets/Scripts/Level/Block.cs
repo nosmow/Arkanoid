e40e6ff9f14407587d286a84263676a41c55471d
nosmow <nosmow.dev@gmail.com>
2024-10-31 09:57:09 -0500

    Modify the Block script
    Added resistance to blocks and feedback when hit

--- Assets/Scripts/Level/Block.cs
+++ Assets/Scripts/Level/Block.cs
@@ -1,16 +1,70 @@
+using System.Collections;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [Header("Block Settings")]
+    [SerializeField] private int resistance; 
+    [SerializeField] private bool isRock;
     [SerializeField] private ParticleSystem particles;
+
+    [Header("Material Settings")]
+    [SerializeField] private Material changeMaterial;
+   // [SerializeField] private float timeChangeMaterial;
+
+    private new Renderer renderer;
+    private Material baseMaterial;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        baseMaterial = renderer.material;
+    }
+
+    #region Methods
+
+    // Coroutine to change block material for a few seconds
+    private IEnumerator CoroutineChangeMaterial()
+    {
+        renderer.material = changeMaterial;
+        yield return new WaitForSeconds(0.2f);
+        renderer.material = baseMaterial;
+    }
+
+    // Decreases resistance
+    private void DecreaseResistance()
+    {
+        if (resistance > 1)
+        {
+            StartCoroutine(CoroutineChangeMaterial());
+        }
+
+        resistance--;
+    }
+
+    // Method to destroy the block
+    private void DestroyBlock()
+    {
+        // Only applies if it is not rock type
+        if (resistance <= 0)
+        {
+            particles.GetComponent<Renderer>().material = renderer.material;
+            Instantiate(particles, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
-            particles.GetComponent<Renderer>().material = this.GetComponent<Renderer>().material;
-            Instantiate(particles, transform.position, Quaternion.identity);
-            Destroy(gameObject);
+            if (!isRock)
+            {
+                DecreaseResistance();
+                DestroyBlock();
+            }
         }
     }
 }
