d729e0841ea462be0c42419c7040133923e9e5b9
nosmow <nosmow.dev@gmail.com>
2024-10-31 09:13:29 -0500

    Modify the level blocks
    Add particles when a block is destroyed

--- Assets/Scripts/Level/Block.cs
+++ Assets/Scripts/Level/Block.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem particles;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
+            particles.GetComponent<Renderer>().material = this.GetComponent<Renderer>().material;
+            Instantiate(particles, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
